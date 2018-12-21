68648495588a9ed8a29f63ae14c56120f2254cc3
hj666c2 <hj666c3@gmail.com>
2018-12-21 18:39:42 +0900

    rank.cs completed (2018 12 21)

--- Program/ToastBot/0 question.cs
+++ Program/ToastBot/0 question.cs
@@ -33,11 +33,11 @@ namespace ToastBot
             .WithIconUrl("https://cdn.discordapp.com/attachments/459706597841567746/521169622246883378/Bot_Profile.png");
     })
     .AddField("?","토스트가 할 수 있는 것들을 보여준다뮤~")
-    .AddField("청소", "채팅방을 청소해준다 ~~핸드폰에서는 아닌 듯 하다~~뮤!")
-    .AddField("빵굽기", "당신의 운을 시험해라. 만약 빵이 적당히 구워지면 투자가 가능한 빵이 된다뮤!")
-    .AddField("투자", "빵굽기로 얻은 빵을 이용해 투자를 합니다 투자한 금액의 1/5~4배까지 얻을 수 있다뮤!")
+    .AddField("청소", "채팅방을 청소해준다뮤! ~~핸드폰에서는 아닌 듯 하다~~뮤!")
+    .AddField("빵굽기", "당신의 운을 시험해봐라뮤~ 만약 빵이 적당히 구워지면 투자가 가능한 빵이 된다뮤!")
+    .AddField("투자", "빵굽기로 얻은 빵을 이용해 투자를 한다뮤~ 투자한 금액의 1/4~4배까지 얻을 수 있다뮤! ~~잘만 하면 다른 결과가 나올지도?~~")
     .AddInlineField("빵은행", "현재 가지고 있는 빵의 갯수를 보여준다뮤!")
-    .AddInlineField("나머지?", "아직 읍서요");
+    .AddInlineField("개발중...", "순위");
             var embed = builder.Build();
             await message.Channel.SendMessageAsync(
                 "",
--- Program/ToastBot/0 rank.cs
+++ Program/ToastBot/0 rank.cs
@@ -13,8 +13,28 @@ namespace ToastBot
     {
         public async Task tnsdnl(SocketMessage message)
         {
+            int arr = 3;
             string[] money = File.ReadAllLines("Players.txt");
-
+            int biggest;
+            string playername = message.Author.Username;
+            string playerid = message.Author.Id.ToString();
+            int he = Array.IndexOf(money, playerid);
+            int rank = 1;
+            biggest = int.Parse(money[he + 1]);
+            int playermoney = biggest;
+            while (arr <= money.Length)
+            {
+                if (int.Parse(money[arr]) > biggest)
+                {
+                    biggest = int.Parse(money[arr]);
+                    rank++;
+                }
+                arr = arr + 3;
+            }
+            int ckdl = biggest - playermoney;
+            var builder = new EmbedBuilder()
+                .AddField(playername + "의 등수는...", rank + "등이다뮤~")
+                .AddField("그리고 1등과의 차이는...", "빵" + ckdl + "개다뮤!");
         }
     }
 }
