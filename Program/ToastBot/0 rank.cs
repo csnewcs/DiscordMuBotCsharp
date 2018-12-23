1cb4f4a160eedd34169bbd3ba133dc5b81b2a6ad
hj666c2 <hj666c3@gmail.com>
2018-12-23 14:04:33 +0900

    2018 12 23 Update
    Rank - Bug Fix

--- Program/ToastBot/0 rank.cs
+++ Program/ToastBot/0 rank.cs
@@ -13,16 +13,21 @@ namespace ToastBot
     {
         public async Task tnsdnl(SocketMessage message)
         {
-            int arr = 3;
+            Random random = new Random();
+            byte red = (byte)random.Next(0, 256);
+            byte green = (byte)random.Next(0, 256);
+            byte blue = (byte)random.Next(0, 256);
+            int arr = 2;
             string[] money = File.ReadAllLines("Players.txt");
             int biggest;
             string playername = message.Author.Username;
             string playerid = message.Author.Id.ToString();
             int he = Array.IndexOf(money, playerid);
+            if (he < 0) biggest = 30;
+            else biggest = int.Parse(money[he + 1]);
             int rank = 1;
-            biggest = int.Parse(money[he + 1]);
             int playermoney = biggest;
-            while (arr <= money.Length)
+            while (arr < money.Length)
             {
                 if (int.Parse(money[arr]) > biggest)
                 {
@@ -34,7 +39,11 @@ namespace ToastBot
             int ckdl = biggest - playermoney;
             var builder = new EmbedBuilder()
                 .AddField(playername + "의 등수는...", rank + "등이다뮤~")
-                .AddField("그리고 1등과의 차이는...", "빵" + ckdl + "개다뮤!");
+                .AddField("그리고 1등과의 차이는...", "빵 " + ckdl + "개다뮤!")
+                .WithColor(new Color(red, green, blue));
+            var embed = builder.Build();
+            await message.Channel.SendMessageAsync("", embed: embed)
+                .ConfigureAwait(false);
         }
     }
 }
