ce6b11864a2b377db4226ccd390e5ce7c5b5090c
hj666c2 <hj666c3@gmail.com>
2018-12-27 21:42:48 +0900

    2018 12 27 Update
    Complete Scoreboard.cs
    Modify README.md

--- Program/ToastBot/0 Scoreboard.cs
+++ Program/ToastBot/0 Scoreboard.cs
@@ -5,14 +5,96 @@ using System.Text;
 using System.Threading.Tasks;
 using Discord;
 using Discord.WebSocket;
+using System.IO;
 
 namespace ToastBot
 {
     class Scoreboard
     {
+        int loop = -1;
         public async Task wjatn(SocketMessage message)
         {
             await message.Channel.SendMessageAsync("지금 이 명령어는 개발중이다뮤! 다음에 버전업을 해서 이것이 완성되면 그때 사용해라 뮤!");
+            string playerid = message.Author.Id.ToString();
+            string[] notepad = File.ReadAllLines("Players.txt");
+            int a = Array.IndexOf(notepad, playerid);
+            if (a < 0)
+            {
+                string all = File.ReadAllText("Players.txt");
+                File.WriteAllText("Players.txt", all + "\n" + playerid + "\n30");
+            }
+            int[] score = new int[5] { 0, 0, 0, 0, 0 };
+            string[] ottff = new string[5] { null, null, null, null, null };
+            string[] output = new string[5] { null, null, null, null, null };
+            score[0] = chltkd();
+            ottff[0] = notepad[loop - 1];
+            loop = -1;
+            score[1] = chltkd();
+            ottff[1] = notepad[loop - 1];
+            loop = -1;
+            score[2] = chltkd();
+            ottff[2] = notepad[loop - 1];
+            loop = -1;
+            score[3] = chltkd();
+            ottff[3] = notepad[loop - 1];
+            loop = -1;
+            score[4] = chltkd();
+            ottff[4] = notepad[loop - 1];
+            File.WriteAllLines("Players.txt", notepad);
+            if (score[0] > 0)
+            { output[0] = ottff[0] + ": " + score[0]; }
+            else output[0] = "사...사람이 읍서요!";
+            if (score[1] > 0)
+            { output[1] = ottff[1] + "\n" + score[1]; }
+            else output[1] = "사...사람이 읍서요!";
+            if (score[2] > 0) output[2] = ottff[2] + "\n" + score[2];
+            else output[2] = "사...사람이 읍서요!";
+            if (score[3] > 0)
+            { output[3] = ottff[3] + "\n" + score[3]; }
+            else output[3] = "사...사람이 읍서요!";
+            if (score[4] > 0)
+            { output[4] = ottff[4] + "\n" + score[4]; }
+            else output[4] = "사...사람이 읍서요!";
+            Random random = new Random();
+            byte red = (byte)random.Next(0, 256);
+            byte green = (byte)random.Next(0, 256);
+            byte blue = (byte)random.Next(0, 256);
+            var builder = new EmbedBuilder()
+                .WithTitle("테스트중...")
+                .WithColor(new Color(red, green, blue))
+                .AddField("1등", "```" + output[0] + "```")
+                .AddField("2등", "```" + output[1] + "```")
+                .AddField("3등", "```" + output[2] + "```")
+                .AddInlineField("4등", "``" + output[3] + "``")
+                .AddInlineField("5등", "``" + output[4] + "``");
+            var embed = builder.Build();
+            await message.Channel.SendMessageAsync(
+                "",
+                embed: embed)
+                .ConfigureAwait(false);
+        }
+        public static string getnickname(ulong playerid)
+        {
+            return playerid.ToString();
+        }
+        public int chltkd()
+        {
+            string[] notepad = File.ReadAllLines("Players.txt");
+            int best = -1;
+            int arr = 2;
+            int length = notepad.Length;
+            while (arr < length)
+            {
+                if (int.Parse(notepad[arr]) > best)
+                {
+                    best = int.Parse(notepad[arr]);
+                    loop = loop + 3;
+                }
+                arr = arr + 3;
+            }
+            notepad[loop] = "0";
+            File.WriteAllLines("Players.txt", notepad);
+            return best;
         }
     }
 }
--- Program/ToastBot/Properties/AssemblyInfo.cs
+++ Program/ToastBot/Properties/AssemblyInfo.cs
@@ -33,4 +33,4 @@ using System.Runtime.InteropServices;
 // 지정되도록 할 수 있습니다.
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("0.2.7")]
-[assembly: AssemblyFileVersion("beta 0.2.2")]
+[assembly: AssemblyFileVersion("beta 0.2.6")]
