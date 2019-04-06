93f1d033350ed89ee5a4c1803c3be6b970afdc8a
PMH Studio / CS <hj666c3@gmail.com>
2019-04-06 12:19:14 +0900

    블랙리스트인지 판별기능 추가

--- Codes/ToastBot/Program.cs
+++ Codes/ToastBot/Program.cs
@@ -44,40 +44,44 @@ namespace 토스트봇
             ulong playerid = message.Author.Id;
             bool isbot = client.GetUser(playerid).IsBot;
             string[] playertext = playertest.Split('!', ' ');
+            ToastBot.blacklist blacklist = new ToastBot.blacklist(); bool black = blacklist.black(playerid.ToString(), message);
             Random random = new Random();
             //명령어 인식 & 실행
             if (isbot != true)
             {
                 if (playertext[0] == "mu" || playertext[0] == "뮤")
                 {
-                    string playerids = playerid.ToString();
-                    string[] wntlr = File.ReadAllLines("playerhave.txt");
-                    string[] array = File.ReadAllLines("Players.txt");
-                    int arrav = Array.IndexOf(array, playerids);
-                    int wntll = Array.IndexOf(wntlr, playerids);
-                    if (wntll < 0)
+                    if (black)
                     {
-                        File.WriteAllText("playerhave.txt", File.ReadAllText("playerhave.txt") + "\n" + playerid + "\n0\n0\n0\n0\n0\n0");
-                        wntlr = File.ReadAllLines("playerhave.txt");
-                        wntll = Array.IndexOf(wntlr, playerids);
-                    }
-                    if (arrav < 0)
-                    {
-                        string a = File.ReadAllText("Players.txt");
-                        string write = a + "\n" + playerid + "\n" + "30\n";
-                        File.WriteAllText("Players.txt", write);
-                    }
+                        string playerids = playerid.ToString();
+                        string[] wntlr = File.ReadAllLines("playerhave.txt");
+                        string[] array = File.ReadAllLines("Players.txt");
+                        int arrav = Array.IndexOf(array, playerids);
+                        int wntll = Array.IndexOf(wntlr, playerids);
+                        if (wntll < 0)
+                        {
+                            File.WriteAllText("playerhave.txt", File.ReadAllText("playerhave.txt") + "\n" + playerid + "\n0\n0\n0\n0\n0\n0");
+                            wntlr = File.ReadAllLines("playerhave.txt");
+                            wntll = Array.IndexOf(wntlr, playerids);
+                        }
+                        if (arrav < 0)
+                        {
+                            string a = File.ReadAllText("Players.txt");
+                            string write = a + "\n" + playerid + "\n" + "30\n";
+                            File.WriteAllText("Players.txt", write);
+                        }
 
-                    if (playertext[1] == "청소") { ToastBot.Clean clean = new ToastBot.Clean(); await clean.clean(message); }
-                    else if (playertext[1] == "토스트") { ToastBot.question question = new ToastBot.question(); await question.anfdmavy(message); }
-                    else if (playertext[1] == "빵굽기") { ToastBot.roastbread roastbread = new ToastBot.roastbread(); await roastbread.Roast(message); }
-                    else if (playertext[1] == "명예의전당") { ToastBot.invest invest = new ToastBot.invest(); await invest.xnwk(message); }
-                    else if (playertext[1] == "빵은행") { ToastBot.bank bank = new ToastBot.bank(); await bank.Bank(message); }
-                    else if (playertext[1] == "순위") { ToastBot.rank rank = new ToastBot.rank(); await rank.tnsdnl(message); }
-                    else if (playertext[1] == "상위권") { ToastBot.Scoreboard scoreboard = new ToastBot.Scoreboard(); await scoreboard.wjatn(message, client); }
-                    else if (playertext[1] == "주식투자") { ToastBot.wntlrxnwk wntlrxnwk = new ToastBot.wntlrxnwk(); await wntlrxnwk.wntlr(message); }
-                    else if (playertext[1] == "송금") { ToastBot.remittance remittance = new ToastBot.remittance(); await remittance.remi(message, client); }
-                    else if (playertext[1] == "초대") { ToastBot.invite invite = new ToastBot.invite(); await invite.inv(message, client); }
+                        if (playertext[1] == "청소") { ToastBot.Clean clean = new ToastBot.Clean(); await clean.clean(message); }
+                        else if (playertext[1] == "토스트") { ToastBot.question question = new ToastBot.question(); await question.anfdmavy(message); }
+                        else if (playertext[1] == "빵굽기") { ToastBot.roastbread roastbread = new ToastBot.roastbread(); await roastbread.Roast(message); }
+                        else if (playertext[1] == "명예의전당") { ToastBot.invest invest = new ToastBot.invest(); await invest.xnwk(message); }
+                        else if (playertext[1] == "빵은행") { ToastBot.bank bank = new ToastBot.bank(); await bank.Bank(message); }
+                        else if (playertext[1] == "순위") { ToastBot.rank rank = new ToastBot.rank(); await rank.tnsdnl(message); }
+                        else if (playertext[1] == "상위권") { ToastBot.Scoreboard scoreboard = new ToastBot.Scoreboard(); await scoreboard.wjatn(message, client); }
+                        else if (playertext[1] == "주식투자") { ToastBot.wntlrxnwk wntlrxnwk = new ToastBot.wntlrxnwk(); await wntlrxnwk.wntlr(message); }
+                        else if (playertext[1] == "송금") { ToastBot.remittance remittance = new ToastBot.remittance(); await remittance.remi(message, client); }
+                        else if (playertext[1] == "초대") { ToastBot.invite invite = new ToastBot.invite(); await invite.inv(message, client); }
+                    }
                 }
             }
         }
---
+++ Codes/ToastBot/blacklist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using Discord.WebSocket;
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace ToastBot
+{
+    class blacklist
+    {
+        public bool black(string playerid,SocketMessage message)
+        {
+            string json = File.ReadAllText(@"user.json");
+            bool turn = true;
+            try
+            {
+                JObject jObject = JObject.Parse(json);
+                turn = (bool)jObject[playerid]["black"];
+            }
+            catch
+            {
+                JObject jObject = JObject.Parse(json);
+                JObject user= JObject.FromObject(new {owner = false, black = false });
+                jObject.Add(playerid, user);
+                File.WriteAllText("user.json",jObject.ToString());
+                turn = false;
+            }
+            if (turn == true) turn = false;
+            else turn = true;
+            return turn;
+        }
+        public Task writeblack(string playerid)
+        {
+            Task.Delay(1);
+            return Task.CompletedTask;
+        }
+    }
+}
