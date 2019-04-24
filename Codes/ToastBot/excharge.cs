b25bb98115a30be7101394d970d377c565ee3595
PMH Studio / CS <hj666c3@gmail.com>
2019-04-24 13:25:37 +0900

    backup

---
+++ Codes/ToastBot/excharge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace ToastBot
+{
+    class excharge
+    {
+        public void breadtomute()
+        {
+            WebClient client = new WebClient();
+            
+        }
+    }
+}
