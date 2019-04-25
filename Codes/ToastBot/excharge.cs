af2a34447cff0eb59cf29f2f59835cd5eb857c84
PMH Studio / CS <hj666c3@gmail.com>
2019-04-25 13:25:37 +0900

    백업이닷

--- Codes/ToastBot/excharge.cs
+++ Codes/ToastBot/excharge.cs
@@ -13,10 +13,12 @@ namespace ToastBot
     {
         public async void breadtomute(SocketMessage message)
         {
+            string playerid = message.Author.Id.ToString();
             WebClient client = new WebClient();
             string url = "https://api.myjson.com/bins/183xhk";
             string readjscoin = await client.DownloadStringTaskAsync(url);
             JObject jscoin = JObject.Parse(readjscoin);
+            int js = int.Parse(jscoin[playerid]["usersCoin"].ToString());
         }
     }
 }
