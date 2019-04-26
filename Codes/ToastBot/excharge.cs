c82f6d622f50f170616c759824563f186d0708e8
PMH Studio / CS <hj666c3@gmail.com>
2019-04-26 13:26:37 +0900

    2019 04 26 점심

--- Codes/ToastBot/excharge.cs
+++ Codes/ToastBot/excharge.cs
@@ -13,20 +13,32 @@ namespace ToastBot
     {
         public async void breadtomute(SocketMessage message,string[] playertext)
         {
+            Console.WriteLine(0);
+
+
             string playerid = message.Author.Id.ToString();
             WebClient client = new WebClient();
             string url = "https://api.myjson.com/bins/183xhk";
-            string readjscoin = "a";
+            Console.WriteLine(1);
+            string readjscoin = "jsonread.json";
             client.DownloadFile(url, readjscoin);
+            readjscoin = File.ReadAllText("jsonread.json");
+            Console.WriteLine(2);
+            Console.WriteLine(readjscoin);
             string[] readcscoin = File.ReadAllLines("Players.txt");
-            JObject jscoin = JObject.Parse(readjscoin); 
-            int js = int.Parse(jscoin[playerid]["usersCoin"].ToString());
+            JObject jscoin = JObject.Parse(readjscoin);
+            Console.WriteLine(3);
+            int js = int.Parse(jscoin[playerid]["UsersCoin"].ToString());
             int cs = int.Parse(readcscoin[Array.IndexOf(readcscoin,playerid) + 1]);
             cs -= int.Parse(playertext[2]);
+            Console.WriteLine(4);
             js += 2 * int.Parse(playertext[2]);
             readcscoin[Array.IndexOf(readcscoin, playerid) + 1] = cs.ToString();
-            jscoin[playerid]["usersCoin"] = js;
-            File.WriteAllLines("",readcscoin);
+            jscoin[playerid]["UsersCoin"] = js;
+            File.WriteAllLines("Players.txt",readcscoin);
+            Console.WriteLine(5);
+            File.WriteAllText("jsonread.json",jscoin.ToString());
+            client.p
             client.UploadFile(url, readjscoin);
             await message.Channel.SendMessageAsync($"송금 완료 ({cs}빵을 {js}MUC로 전환 완료)");
         }
